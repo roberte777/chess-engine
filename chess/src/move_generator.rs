e631d693544cd2f94de05d5ac0c648f64d9257d2
roberte777 <robewilkes@yahoo.com>
2024-05-07 18:48:59 -0500

    feat: improved tests

--- chess/src/move_generator.rs
+++ chess/src/move_generator.rs
@@ -753,6 +753,23 @@ mod tests {
         run_perft_test(fen, vec![48, 2_039, 97_862, 4_085_603])
     }
 
+    #[test]
+    fn test_perft_position_3() {
+        let fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+        run_perft_test(fen, vec![14, 191, 2812, 43238, 674624, 11_030_083])
+    }
+
+    #[test]
+    fn test_perft_position_4() {
+        let fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+        run_perft_test(fen, vec![6, 264, 9467, 422333, 15_833_292])
+    }
+    #[test]
+    fn test_perft_position_5() {
+        let fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+        run_perft_test(fen, vec![44, 1486, 62379, 2_103_487])
+    }
+
     // #[test]
     // fn test_broken() {
     //     let fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
